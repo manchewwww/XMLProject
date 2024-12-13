3cb171db8dc0af2a2ec4ffc6b48d784efdbc5a94
manchewwww <georgimanchev2003@gmail.com>
2024-12-13 17:01:03 +0200

    Add second part of project

--- DOM/scripts/xml.js
+++ DOM/scripts/xml.js
@@ -2,19 +2,16 @@ function createXMLDocument() {
     const documentImplementation = document.implementation;
     const doc = documentImplementation.createDocument(null, "autoParts", null);
 
-    // Помощна функция за създаване на елемент с текст
     function createElementWithText(name, text) {
         const element = doc.createElement(name);
         element.textContent = text;
         return element;
     }
 
-    // Добавяне на атрибути към главния елемент
     const autoPartsElement = doc.documentElement;
     autoPartsElement.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
     autoPartsElement.setAttribute("xsi:noNamespaceSchemaLocation", "../../XMLSchema/Schema.xsd");
 
-    // Създаване на <storage> с <parts>
     const storageElement = doc.createElement("storage");
     const partsElement = doc.createElement("parts");
     partsElement.setAttribute("code", "P001");
@@ -22,7 +19,6 @@ function createXMLDocument() {
     partsElement.setAttribute("warranty", "5 years");
     partsElement.setAttribute("manufacturerDate", "2022-01-15");
 
-    // Добавяне на под-елементи към <parts>
     partsElement.appendChild(createElementWithText("name", "Oil filter"));
     partsElement.appendChild(createElementWithText("category", "Filters"));
     partsElement.appendChild(createElementWithText("brand", "Bosch"));
@@ -39,7 +35,6 @@ function createXMLDocument() {
     storageElement.appendChild(partsElement);
     autoPartsElement.appendChild(storageElement);
 
-    // Създаване на <suppliers> с <supplier>
     const suppliersElement = doc.createElement("suppliers");
     const supplierElement = doc.createElement("supplier");
     supplierElement.setAttribute("supplierID", "S001");
